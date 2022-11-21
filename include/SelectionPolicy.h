7d2dce2e2b5599b696d39708d6e4863b5e729883
Or Saada <saadaor10@gmail.com>
2022-11-21 23:27:13 +0200

    debug

--- include/SelectionPolicy.h
+++ include/SelectionPolicy.h
@@ -5,11 +5,7 @@
 
 class SelectionPolicy {
      public:
-<<<<<<< HEAD
         virtual int select(std::vector<int> , Graph &, Agent, std::vector<int>) = 0;
-=======
-        virtual int select(std::vector<int> , Graph &, Agent, vector<int>) = 0;
->>>>>>> master
         virtual SelectionPolicy* clone() =0 ;
  };
 
